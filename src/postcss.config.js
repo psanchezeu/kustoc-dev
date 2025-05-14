38a694ed38888d4c0702b055e7423e6819399100
psanchezeu <psanchez.audidata@gmail.com>
2025-05-14 11:46:45 +0200

    feat: configurar PostCSS y Tailwind
    - Añade dependencias y archivos de configuración
    - Integra Tailwind en el CSS global

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// Usar una función para poder proporcionar la configuración
+export default () => {
+  return {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+}
