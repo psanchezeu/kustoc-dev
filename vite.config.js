4516d2d1e0e96006c8450cbdeed2b7c9d662e747
psanchezeu <psanchez.audidata@gmail.com>
2025-05-14 11:55:20 +0200

    Añadir configuración de Vite en la raíz para despliegue

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  },
+  build: {
+    outDir: './dist',
+    target: 'es2020'
+  },
+  root: './src'
+});
